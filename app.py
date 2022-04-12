0de8eec839589887315e673d3e56bf19f6b27f0d
aamirsabri <aamir.kashiri@gmail.com>
2022-04-12 23:07:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -269,8 +269,9 @@ def main():
     updater.start_webhook(
         listen="0.0.0.0",
         port=8443,
-        url_path=TOKEN)        
-    updater.bot.setWebhook('https://zakatbot-telegram.herokuapp.com/' + TOKEN)
+        url_path=TOKEN,
+        webhook_url='https://zakatbot-telegram.herokuapp.com/' + TOKEN)        
+    
     updater.idle()
 
 if __name__ =="__main__":
